0f65ec18c78e9bf8cd60164a118fe7cf2e4b894a
Mostafa Khaled <Mostafa1810751@outlook.com>
2021-11-24 03:26:29 +0200

    edit in models

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,7 @@
 class Product {
   final String pid;
   final String name;
+  final String pic;
 
-  Product({required this.pid, required this.name});
+  Product({required this.pid, required this.name, required this.pic});
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,7 @@
 class User {
   final String uid;
   final String name;
+  final String pic;
 
-  User({required this.uid, required this.name});
+  User({required this.uid, required this.name, required this.pic});
 }
