72714e9cbd4ff8613c99de46cc3f5acd8a0e7c58
sherif elmotayam <sherifelmotayam@gmail.com>
2021-12-08 18:33:53 +0200

    update person class

---
+++ lib/models/person.dart
@@ -0,0 +1,17 @@
+class Person {
+  final String uid;
+  final String name;
+  final String pic;
+  final String gender;
+  final int phoneNumber;
+  final String type;
+
+  Person({
+    required this.uid,
+    required this.name,
+    required this.pic,
+    required this.gender,
+    required this.phoneNumber,
+    required this.type,
+  });
+}
--- lib/models/user.dart
+++
@@ -1,7 +0,0 @@
-class User {
-  final String uid;
-  final String name;
-  final String pic;
-
-  User({required this.uid, required this.name, required this.pic});
-}
