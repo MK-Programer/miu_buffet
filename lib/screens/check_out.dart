ab4ea6511a72b299bc09132f62677385916009b2
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2021-12-13 01:11:29 +0200

    update checkout

--- lib/screens/check_out.dart
+++ lib/screens/check_out.dart
@@ -4,30 +4,39 @@ import 'package:miu_food_court/shared/widgets/bottom_bar.dart';
 import 'package:miu_food_court/shared/widgets/side_menu_bar.dart';
 import 'package:miu_food_court/shared/widgets/top_bar.dart';
 
-class Checkout extends StatelessWidget {
+class Checkout extends StatefulWidget {
   const Checkout({Key? key}) : super(key: key);
 
   @override
+  _CheckoutState createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<Checkout> {
+  final List<String> options = ['By Myself', 'By delivery'];
+  String _deliveryOption = 'By Myself';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: SideBar(),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(50.0),
-          child: TopBar('Check Out'),
-        ),
-        bottomNavigationBar: BottomBar(),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: ListView(children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Row(
+      drawer: SideBar(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: TopBar('Check out'),
+      ),
+      bottomNavigationBar: BottomBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ListView(children: [
+            Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text(
-                      'Payment Method',
-                      style: textStyle2,
+                    Text(
+                      'Delivery Method',
+                      style: textStyle3.copyWith(
+                        fontSize: fontSizeM,
+                      ),
                     ),
                     TextButton(
                       onPressed: () {},
@@ -35,105 +44,61 @@ class Checkout extends StatelessWidget {
                         'Change',
                         style: TextStyle(
                           fontSize: fontSizeS,
+                          color: red,
                         ),
                       ),
                     )
                   ],
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Delivery Method',
-                        style: textStyle2,
+                const SizedBox(height: 2.0),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.home_outlined,
+                      size: iconSize,
+                      color: red,
+                    ),
+                    Text(
+                      'Room 307',
+                      style: TextStyle(
+                        fontSize: fontSizeS,
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Change',
-                          style: TextStyle(
-                            fontSize: fontSizeS,
-                          ),
-                        ),
-                      )
-                    ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Delivery Options',
+                  style: textStyle3.copyWith(
+                    fontSize: fontSizeM,
                   ),
-                  const SizedBox(height: 2.0),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.home,
-                        color: red,
-                      ),
-                      Text(
-                        'Room 307',
-                        style: TextStyle(
-                          fontSize: fontSizeS,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20.0),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Delivery Options',
-                        style: textStyle2,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Change',
-                          style: TextStyle(
-                            fontSize: fontSizeS,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 2.0),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.nordic_walking_sharp,
-                        color: red,
-                      ),
-                      Text(
-                        'I’ll pick it up myself',
-                        style: TextStyle(
-                          fontSize: fontSizeS,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.pedal_bike,
-                        color: red,
-                      ),
-                      Text(
-                        'By courier',
-                        style: TextStyle(
-                          fontSize: fontSizeS,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20.0),
-            ]),
-          ),
-        ));
+                ),
+                const SizedBox(height: 2.0),
+                DropdownButtonFormField<String>(
+                  value: _deliveryOption,
+                  decoration: textInputDecoration,
+                  items: options.map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text('$e'),
+                    );
+                  }).toList(),
+                  // if the onChanged callback is null or the list of items is null
+                  // then the dropdown button will be disabled
+                  onChanged: (val) =>
+                      setState(() => _deliveryOption = val as String),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+          ]),
+        ),
+      ),
+    );
   }
 }
