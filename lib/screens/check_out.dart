bfdb04c94a87f099de3656f41718811119f7fa93
Mostafa Khaled <Mostafa1810751@outlook.com>
2021-12-13 03:38:23 +0200

    update check_out

--- lib/screens/check_out.dart
+++ lib/screens/check_out.dart
@@ -26,77 +26,80 @@ class _CheckoutState extends State<Checkout> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: ListView(children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Delivery Method',
-                      style: textStyle3.copyWith(
-                        fontSize: fontSizeM,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Change',
-                        style: TextStyle(
-                          fontSize: fontSizeS,
-                          color: red,
+          child: ListView(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Delivery Method',
+                        style: textStyle3.copyWith(
+                          fontSize: fontSizeM,
                         ),
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 2.0),
-                Row(
-                  children: const [
-                    Icon(
-                      Icons.home_outlined,
-                      size: iconSize,
-                      color: red,
-                    ),
-                    Text(
-                      'Room 307',
-                      style: TextStyle(
-                        fontSize: fontSizeS,
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Change',
+                          style: TextStyle(
+                            fontSize: fontSizeS,
+                            color: red,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Delivery Options',
-                  style: textStyle3.copyWith(
-                    fontSize: fontSizeM,
+                    ],
                   ),
-                ),
-                const SizedBox(height: 2.0),
-                DropdownButtonFormField<String>(
-                  value: _deliveryOption,
-                  decoration: textInputDecoration,
-                  items: options.map((e) {
-                    return DropdownMenuItem(
-                      value: e,
-                      child: Text('$e'),
-                    );
-                  }).toList(),
-                  // if the onChanged callback is null or the list of items is null
-                  // then the dropdown button will be disabled
-                  onChanged: (val) =>
-                      setState(() => _deliveryOption = val as String),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20.0),
-          ]),
+                  const SizedBox(height: 2.0),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.home_outlined,
+                        size: iconSize,
+                        color: red,
+                      ),
+                      Text(
+                        'Room 307',
+                        style: TextStyle(
+                          fontSize: fontSizeS,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Delivery Options',
+                    style: textStyle3.copyWith(
+                      fontSize: fontSizeM,
+                    ),
+                  ),
+                  const SizedBox(height: 2.0),
+                  DropdownButtonFormField<String>(
+                    value: _deliveryOption,
+                    decoration: textInputDecoration,
+                    items: options.map(
+                      (e) {
+                        return DropdownMenuItem(
+                          value: e,
+                          child: Text('$e'),
+                        );
+                      },
+                    ).toList(),
+                    // if the onChanged callback is null or the list of items is null
+                    // then the dropdown button will be disabled
+                    onChanged: (val) =>
+                        setState(() => _deliveryOption = val as String),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
