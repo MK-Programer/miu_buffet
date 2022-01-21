0bdf625e7415e077d74ecff848ea499b89e8b1f0
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2022-01-21 16:57:11 +0200

    sign_in

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:miu_food_court/services/auth.dart';
 import 'package:miu_food_court/shared/variables/constants.dart';
+import 'package:miu_food_court/shared/widgets/loading.dart';
 import 'package:miu_food_court/shared/widgets/top_bar.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
@@ -12,95 +13,147 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formkey = GlobalKey<FormState>();
+  bool loading = false;
+  String errmsg = '';
 
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: TopBar('Signin'),
-      ),
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: ListView(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'SignIn',
-                    style: TextStyle(
-                      fontSize: fontSize30,
-                    ),
-                  ),
-                ],
-              ),
-              Form(
-                key: _formkey,
-                child: Column(
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50.0),
+              child: TopBar('Signin'),
+            ),
+            body: SafeArea(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: ListView(
                   children: [
-                    TextFormField(
-                      decoration: textInputDecoration.copyWith(
-                        hintText: 'Email',
-                        prefixIcon: Icon(
-                          Icons.person_outlined,
-                          color: grey,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'SignIn',
+                          style: TextStyle(
+                            fontSize: fontSize30,
+                          ),
                         ),
+                      ],
+                    ),
+                    Form(
+                      key: _formkey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: emailController,
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Email',
+                              prefixIcon: Icon(
+                                Icons.email_outlined,
+                                color: grey,
+                              ),
+                            ),
+                            validator: (val) =>
+                                val!.isEmpty ? 'Enter an email' : null,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Password',
+                              prefixIcon: Icon(
+                                Icons.password_outlined,
+                                color: grey,
+                              ),
+                            ),
+                            validator: (val) =>
+                                val!.isEmpty ? 'Enter the password' : null,
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '$errmsg',
+                            style: TextStyle(
+                              color: red,
+                              fontSize: fontSize18,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              dynamic result = await _auth.signin(
+                                  email: emailController.text.trim(),
+                                  password: passwordController.text);
+                              if (!result) {
+                                setState(() {
+                                  this.errmsg =
+                                      'Cann\'t Signin with those credintials';
+                                  loading = false;
+                                });
+                              } else {
+                                setState(() {
+                                  this.errmsg = '';
+                                  loading = true;
+                                });
+
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/adminhome', (route) => false);
+                              }
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: fontSize15,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size.fromHeight(40),
+                              primary:
+                                  red, // fromHeight use double.infinity as width and 40 is the height
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: SignInButton(
+                              Buttons.GoogleDark,
+                              onPressed: () async {
+                                try {
+                                  dynamic result =
+                                      await _auth.signinWithGoogle();
+                                  if (result == false)
+                                    setState(() {
+                                      this.errmsg =
+                                          'Cann\'t Signin with those credintials';
+                                      loading = false;
+                                    });
+                                  else {
+                                    setState(() {
+                                      this.errmsg = '';
+                                      loading = true;
+                                    });
+                                    Navigator.of(context)
+                                        .pushNamedAndRemoveUntil(
+                                            '/home', (route) => false);
+                                  }
+                                } catch (e) {
+                                  print('error ${e.toString()}');
+                                  return null;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      validator: (val) =>
-                          val!.isEmpty ? 'Enter an email' : null,
-                      onChanged: (val) {},
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    TextFormField(
-                      decoration: textInputDecoration.copyWith(
-                        hintText: 'Password',
-                        prefixIcon: Icon(
-                          Icons.lock_outlined,
-                          color: grey,
-                        ),
-                      ),
-                      validator: (val) =>
-                          val!.isEmpty ? 'Enter the password' : null,
-                      onChanged: (val) {},
-                    ),
-                    const SizedBox(
-                      height: 15.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: fontSize15,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size.fromHeight(40),
-                        primary:
-                            red, // fromHeight use double.infinity as width and 40 is the height
-                      ),
-                    ),
-                    SignInButton(
-                      Buttons.Google,
-                      onPressed: () async {
-                        try {
-                          await _auth.signinWithGoogle();
-                        } catch (e) {
-                          return null;
-                        }
-                      },
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
