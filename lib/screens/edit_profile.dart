1d32f534ed0efe53b1f6e5be0410da1ddbed4a95
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2022-01-21 16:55:55 +0200

    edit profile

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,99 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:miu_food_court/models/user.dart';
+import 'package:miu_food_court/services/auth.dart';
+import 'package:miu_food_court/services/data_service.dart';
 import 'package:miu_food_court/shared/variables/constants.dart';
 import 'package:miu_food_court/shared/widgets/bottom_bar.dart';
+import 'package:miu_food_court/shared/widgets/loading.dart';
 import 'package:miu_food_court/shared/widgets/side_menu_bar.dart';
-import 'package:miu_food_court/shared/widgets/admin_side_menu_bar.dart';
 import 'package:miu_food_court/shared/widgets/top_bar.dart';
 
-bool Admin_Visibilty=true;
-class EditProfile extends StatelessWidget {
+class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
 
   @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  AuthService _auth = AuthService();
+  String phone = '';
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer:  Admin_Visibilty?AdminSideBar():SideBar() ,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: TopBar('Edit Profile'),
-      ),
-      bottomNavigationBar: Admin_Visibilty? null:BottomBar(),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return StreamBuilder<UserData?>(
+      stream: AuthService().user,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          UserData? userData = snapshot.data;
+          return Scaffold(
+            drawer: SideBar(),
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50.0),
+              child: TopBar('Edit Profile'),
+            ),
+            bottomNavigationBar: BottomBar(),
+            body: ListView(
               children: [
-                Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage(
-                      'assets/pictures/person.png',
-                    ),
-                    radius: 70.0,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.camera_alt_outlined,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            userData!.picture ?? 'assets/pictures/person.png',
+                          ),
+                          radius: 70.0,
+                        ),
                       ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-                const Divider(
-                  height: 50.0,
-                  color: black,
-                ),
-                const Text(
-                  'FULL NAME',
-                  style: textStyle3,
-                ),
-                const SizedBox(height: 10.0),
-                const Text(
-                  'Essam Eliwa',
-                  style: textStyle1,
-                ),
-                const SizedBox(height: 30.0),
-                const Text(
-                  'EMAIL',
-                  style: textStyle3,
-                ),
-                const SizedBox(height: 10.0),
-                const Text(
-                  'essam.eliwa@miuegypt.edu.eg',
-                  style: textStyle2,
-                ),
-                const SizedBox(height: 30.0),
-                const Text(
-                  'PHONE NUMBER',
-                  style: textStyle3,
-                ),
-                const SizedBox(height: 10.0),
-                TextFormField(
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                  keyboardType: TextInputType.number,
-                  validator: (val) =>
-                      val!.isEmpty ? 'Enter the phone number please...' : null,
-                  onChanged: (val) {},
-                  initialValue: '01020820065',
-                  decoration: textInputDecoration.copyWith(
-                    hintText: 'Enter your number...',
-                    suffixIcon: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.edit,
-                        color: red,
+                      const Divider(
+                        height: 50.0,
+                        color: black,
                       ),
-                    ),
+                      const Text(
+                        'FULL NAME',
+                        style: textStyle3,
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        '${userData.name}',
+                        style: textStyle1,
+                      ),
+                      const SizedBox(height: 30.0),
+                      const Text(
+                        'EMAIL',
+                        style: textStyle3,
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        '${userData.email}',
+                        style: textStyle2,
+                      ),
+                      const SizedBox(height: 30.0),
+                      const Text(
+                        'PHONE NUMBER',
+                        style: textStyle3,
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        initialValue: '${userData.phoneNumber}',
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        onChanged: (val) {
+                          phone = val;
+                        },
+                        keyboardType: TextInputType.number,
+                        validator: (val) => val!.isEmpty
+                            ? 'Enter the phone number please...'
+                            : null,
+                        decoration: textInputDecoration.copyWith(
+                          hintText: 'Enter your number...',
+                          suffixIcon: IconButton(
+                            onPressed: () async {
+                              String result = await _auth.getPref();
+                              DatabaseService db = DatabaseService(uid: result);
+                              db.savePhone(phone);
+                            },
+                            icon: Icon(
+                              Icons.edit,
+                              color: red,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
+          );
+        } else
+          return Loading();
+      },
     );
   }
 }
