70c045f49a3c153cc4f2c4727ddd8c92e1248ec0
Mostafa Khaled <Mostafa1810751@outlook.com>
2022-01-24 01:04:03 +0200

    cart model

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,11 +1,14 @@
 class Cart {
+  String uid;
+  int pid;
   String picture;
   String name;
   String sugar;
   int quantity;
   String price;
   String totalPrice;
+  String category;
 
-  Cart(this.picture, this.name, this.sugar, this.quantity, this.price,
-      this.totalPrice);
+  Cart(this.uid, this.pid, this.picture, this.name, this.sugar, this.quantity,
+      this.price, this.totalPrice, this.category);
 }
