fd5d712c84760d094a5567f33a239809f59e2c95
Mostafa Khaled <Mostafa1810751@outlook.com>
2022-01-24 01:04:22 +0200

    products model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,7 +1,18 @@
 class Product {
+  int pid;
   String picture;
   String name;
   String price;
   String category;
-  Product(this.picture, this.name, this.price, this.category);
+  Product(this.pid, this.picture, this.name, this.price, this.category);
+
+  factory Product.fromJson(Map json) {
+    return Product(
+      int.parse(json['data']['pid']),
+      json['data']['picture'] ?? '',
+      json['data']['name'] ?? '',
+      json['data']['price'] ?? '',
+      json['data']['category'] ?? '',
+    );
+  }
 }
