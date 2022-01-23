f15bc8ee29af54250de0953764ad5442da38c2d8
Mostafa Khaled <Mostafa1810751@outlook.com>
2022-01-24 01:04:39 +0200

    about screen

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:miu_food_court/shared/variables/constants.dart';
 import 'package:miu_food_court/shared/widgets/bottom_bar.dart';
-import 'package:miu_food_court/shared/widgets/rich_text.dart';
 import 'package:miu_food_court/shared/widgets/side_menu_bar.dart';
 import 'package:miu_food_court/shared/widgets/top_bar.dart';
 
@@ -24,13 +23,15 @@ class Aboutus extends StatelessWidget {
             children: [
               Center(
                 child: const Text(
-                  'What is MIU Buffett?',
+                  'What is MIU Buffet?',
                   style: textStyle2,
                 ),
               ),
-              const SizedBox(height: 5.0),
+              const SizedBox(
+                height: 5.0,
+              ),
               Text(
-                'MIU Buffett is a system that facilitates the process of ordering a drink or snacks from the buffet. Most of the professors face a problem that they should leave their rooms and go for finding the officers in the buffet to order and if they are not there, doctors can not order anything untill they returned back , So the system is going to make this process quick and easy by opening the application and just choosing their order and his room, then the officer will get a notification by the order, information about the doctor and his room. And when he prepaired the order he will deliver it to the doctor in his room without letting the doctor make any effort.',
+                'MIU Buffet is a system that facilitates the process of ordering a drink or snacks from the buffet. Most of the professors face a problem that they should leave their rooms and go for finding the officers in the buffet to order and if they are not there, doctors can not order anything untill they returned back , So the system is going to make this process quick and easy by opening the application and just choosing their order and his room, then the officer will get a notification by the order, information about the doctor and his room. And when he prepaired the order he will deliver it to the doctor in his room without letting the doctor make any effort.',
                 textAlign: TextAlign.center,
                 style: textStyle3.copyWith(
                   fontSize: fontSize10,
@@ -78,10 +79,14 @@ class Aboutus extends StatelessWidget {
               ),
               Center(
                 child: const ListTile(
-                  title:
-                      Text('Amr Khaled Shaltout', textAlign: TextAlign.center),
-                  subtitle: Text('Amr1812597@miuegypt.edu.eg',
-                      textAlign: TextAlign.center),
+                  title: Text(
+                    'Amr Khaled Shaltout',
+                    textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    'Amr1812597@miuegypt.edu.eg',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
               const Divider(
@@ -98,9 +103,14 @@ class Aboutus extends StatelessWidget {
               ),
               Center(
                 child: const ListTile(
-                  title: Text('Mostafa Khaled', textAlign: TextAlign.center),
-                  subtitle: Text('Mostafa1810751@miuegypt.edu.eg',
-                      textAlign: TextAlign.center),
+                  title: Text(
+                    'Mostafa Khaled',
+                    textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    'Mostafa1810751@miuegypt.edu.eg',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
               const Divider(
@@ -117,9 +127,14 @@ class Aboutus extends StatelessWidget {
               ),
               Center(
                 child: const ListTile(
-                  title: Text('Sherif Elmotayam', textAlign: TextAlign.center),
-                  subtitle: Text('Sherif1809806@miuegypt.edu.eg',
-                      textAlign: TextAlign.center),
+                  title: Text(
+                    'Sherif Elmotayam',
+                    textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    'Sherif1809806@miuegypt.edu.eg',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
               const Divider(
@@ -136,9 +151,14 @@ class Aboutus extends StatelessWidget {
               ),
               Center(
                 child: const ListTile(
-                  title: Text('Ahmed Gamil', textAlign: TextAlign.center),
-                  subtitle: Text('Ahmed1801759@miuegypt.edu.eg',
-                      textAlign: TextAlign.center),
+                  title: Text(
+                    'Ahmed Gamil',
+                    textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    'Ahmed1801759@miuegypt.edu.eg',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ],
