48af591d1ed2831f2201cb86a5d6a007059307c0
Mostafa Khaled <Mostafa1810751@outlook.com>
2022-01-24 01:04:14 +0200

    update orders

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,8 +1,22 @@
 class Order {
+  String uid;
+  int pid;
   String picture;
   String name;
   int quantity;
   String totalPrice;
 
-  Order(this.picture, this.name, this.quantity, this.totalPrice);
+  Order(this.uid, this.pid, this.picture, this.name, this.quantity,
+      this.totalPrice);
+
+  factory Order.fromJson(Map json) {
+    return Order(
+      json['data']['uid'],
+      int.parse(json['data']['pid']),
+      json['data']['picture'] ?? '',
+      json['data']['name'] ?? '',
+      json['data']['quantity'],
+      json['data']['price'] ?? '',
+    );
+  }
 }
