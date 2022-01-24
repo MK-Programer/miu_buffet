b1bd258a9ee582b61304aab13c97861c4088dcb9
Mostafa Khaled <Mostafa1810751@outlook.com>
2022-01-24 04:22:10 +0200

    final commit

--- lib/screens/add_office.dart
+++ lib/screens/add_office.dart
@@ -1,85 +1,85 @@
-import 'package:flutter/material.dart';
-import 'package:miu_food_court/shared/variables/constants.dart';
-import 'package:miu_food_court/shared/widgets/admin_side_menu_bar.dart';
-import 'package:miu_food_court/shared/widgets/admin_top_bar.dart';
+// import 'package:flutter/material.dart';
+// // import 'package:miu_food_court/shared/variables/constants.dart';
+// import 'package:miu_food_court/shared/widgets/admin_side_menu_bar.dart';
+// import 'package:miu_food_court/shared/widgets/admin_top_bar.dart';
 
-class AddOffice extends StatefulWidget {
-  AddOffice({Key? key}) : super(key: key);
+// class AddOffice extends StatefulWidget {
+//   AddOffice({Key? key}) : super(key: key);
 
-  @override
-  _AddOfficeState createState() => _AddOfficeState();
-}
+//   @override
+//   _AddOfficeState createState() => _AddOfficeState();
+// }
 
-class _AddOfficeState extends State<AddOffice> {
-  @override
-  final controllerUsername = TextEditingController();
-  final controllerPassword = TextEditingController();
-  final controllerEmail = TextEditingController();
+// class _AddOfficeState extends State<AddOffice> {
+//   @override
+//   final controllerUsername = TextEditingController();
+//   final controllerPassword = TextEditingController();
+//   final controllerEmail = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: AdminSideBar(),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(50.0),
-          child: AdminTopBarOffice('View Ofiicers'),
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(
-                  height: 5,
-                ),
-                Center(
-                  child:
-                      const Text('Add Office', style: TextStyle(fontSize: 16)),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: controllerEmail,
-                  keyboardType: TextInputType.emailAddress,
-                  textCapitalization: TextCapitalization.none,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFA83332))),
-                      labelText: 'E-mail'),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: controllerPassword,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                  textCapitalization: TextCapitalization.none,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFA83332))),
-                      labelText: 'Password'),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  height: 100,
-                  child: TextButton(
-                    child: const Text('Add ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFFA83332))),
-                    onPressed: () {},
-                  ),
-                )
-              ],
-            ),
-          ),
-        ));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         drawer: AdminSideBar(),
+//         appBar: PreferredSize(
+//           preferredSize: Size.fromHeight(50.0),
+//           child: AdminTopBarOffice('View Ofiicers'),
+//         ),
+//         body: Center(
+//           child: SingleChildScrollView(
+//             padding: const EdgeInsets.all(8),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 SizedBox(
+//                   height: 5,
+//                 ),
+//                 Center(
+//                   child:
+//                       const Text('Add Office', style: TextStyle(fontSize: 16)),
+//                 ),
+//                 SizedBox(
+//                   height: 25,
+//                 ),
+//                 TextField(
+//                   controller: controllerEmail,
+//                   keyboardType: TextInputType.emailAddress,
+//                   textCapitalization: TextCapitalization.none,
+//                   autocorrect: false,
+//                   decoration: InputDecoration(
+//                       border: OutlineInputBorder(
+//                           borderSide: BorderSide(color: Color(0xFFA83332))),
+//                       labelText: 'E-mail'),
+//                 ),
+//                 SizedBox(
+//                   height: 25,
+//                 ),
+//                 TextField(
+//                   controller: controllerPassword,
+//                   obscureText: true,
+//                   keyboardType: TextInputType.text,
+//                   textCapitalization: TextCapitalization.none,
+//                   autocorrect: false,
+//                   decoration: InputDecoration(
+//                       border: OutlineInputBorder(
+//                           borderSide: BorderSide(color: Color(0xFFA83332))),
+//                       labelText: 'Password'),
+//                 ),
+//                 SizedBox(
+//                   height: 8,
+//                 ),
+//                 Container(
+//                   height: 100,
+//                   child: TextButton(
+//                     child: const Text('Add ',
+//                         style: TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             color: Color(0xFFA83332))),
+//                     onPressed: () {},
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ));
+//   }
+// }
